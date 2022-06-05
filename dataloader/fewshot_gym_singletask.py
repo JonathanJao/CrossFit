b9171ad3b2e69f3778b75653d4de9d3268711436
Jonathan Jao <fondyjao@gmail.com>
2022-06-05 17:34:27 -0500

    utf-8 decode

--- dataloader/fewshot_gym_singletask.py
+++ dataloader/fewshot_gym_singletask.py
@@ -31,7 +31,7 @@ class NLPFewshotGymSingleTaskData(object):
 
         # train_examples = []
         for line in lines:
-            d = line.strip().split("\t")
+            d = line.decode('utf-8').strip().split("\t")
             self.data.append((d[0], d[1:]))
             
 
