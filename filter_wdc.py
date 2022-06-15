0e785298a79f4409fe37a4ccbdf4a431a9dfa243
Jonathan Jao <fondyjao@gmail.com>
2022-06-15 00:20:41 -0500

    slurm folder and filter_wdc.py

---
+++ filter_wdc.py
@@ -0,0 +1,20 @@
+import json
+import sys
+import pandas as pd
+
+if len(sys.argv) == 2:
+
+    divdom = sys.argv[1]
+    files = json.loads(open(divdom.read).read())['train']
+
+    obj_list = []
+    for filename in files:
+        with open(filename, 'r') as json_file:
+            obj_list += [json.loads(json_str) for json_str in list(json_file)]
+
+    df = pd.DataFrame(obj_list)
+    print (len(obj_list))
+    df = df[['input', 'output']]
+    df.to_csv("big.tsv", sep="\t", index=False, header=None)
+else:
+    print ("Usage: filter_hr.py file_list.json")
